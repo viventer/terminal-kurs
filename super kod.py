2b69325d285886ed34e22943d0f264f980de3df8
viventer <viventermail@gmail.com>
2023-10-01 20:08:34 +0200

    dodanie pliku

---
+++ super kod.py
@@ -0,0 +1,13 @@
+print("Hello World")
+print("Hello World")
+if true:
+	if true == true:
+		print("Hello World")
+		if false == 0:
+			while True:
+				while "a" < "b":
+					if "a":
+						print("Hello World")
+print("super kod powyżej")
+print("super kod powyżej")
+print("Hello World")
